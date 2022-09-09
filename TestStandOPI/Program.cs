7113bea726e22da20c8e9c34542b3eee4fde3421
Margir Róbert <marrob50hz@gmail.com>
2022-09-09 14:49:25 +0200

    update

---
+++ TestStandOPI/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace TestStandOPI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new App().MainForm);
+        }
+
+
+
+        public class App
+        {
+            public Form MainForm;
+
+            public App()
+            {
+
+                MainForm = new MainForm();
+
+            }
+        
+        }
+    }
+}
--- readme.txt
+++ readme.txt
@@ -3,3 +3,15 @@ https://www.codeproject.com/Articles/302873/Remote-Controlling-Instrument-Over-L
 
 TCPClient vs Socket in C#
 https://stackoverflow.com/questions/685995/tcpclient-vs-socket-in-c-sharp
+
+
+/*** Debug From TestStand ***/
+TestStand 2017
+Visual Studio 2019 v 2022
+
+1. Tegyél töréspontot abba a C# metódusba ahol meg szeretnél álni
+2. Válaszd a Debug-> Attach to Process menüpontot majd válaszd a SeqEdit.exe-ét a listából
+3. Indisd e TestStandbol a progit majd SetpInto-val lépj bele... 
+(elofordulahat hogy feldob egy ilyen ablakot hogy: Warning ... Could not find DIA CLSID for this version of Visual Studio)
+
+
