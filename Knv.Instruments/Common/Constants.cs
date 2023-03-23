430b663ce38739b11979ad561c1b42a780a3cb90
marrob <marrob50hz@gmail.com>
2023-03-23 20:20:25 +0100

    Multi AO0/AO1 Simul

---
+++ Knv.Instruments/Common/Constants.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Knv.Instruments.Common
+{
+    using System;
+
+    internal class Constants
+    {
+        public static string LogRootDirecotry = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Knv.Instruments.Log\\";
+
+    }
+}
