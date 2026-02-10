d7ad7bbc89840fd812851c322b4c7ae321e1a5e2
marrob <marrob50hz@gmail.com>
2026-02-10 17:58:03 +0100

    add M9185 Support

---
+++ Knv.Instr.DAC.M9185A.Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Knv.Instr.DAC.M9185A.Demo
+{
+    internal class Program
+    {
+        const string INSTRUMENT_NAME = "PXI14::13::0::INSTR";
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (M9185 dac = new M9185(INSTRUMENT_NAME))
+                {
+                    dac.Open();
+                    Console.WriteLine(dac.Description());
+                    dac.VoltageOutputConfig(0, 5);
+
+                    dac.VoltageOutputConfig(2, 5);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Done - Press Enter to Exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ Knv.Instr.DAC.M9185A/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Knv.Instr.DAC.M9185A")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Knv.Instr.DAC.M9185A")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("D28C2267-F919-4751-9E87-AE182B68E0FF")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
